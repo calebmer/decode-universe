b473751737e59239a027930ff5cf9733e5917199
Caleb Meredith <calebmeredith8@gmail.com>
2017-04-06 16:17:42 -0400

    add rough participants

---
+++ repos/studio-ui/src/StudioParticipant.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class StudioParticipant extends React.Component<{}, {}> {
+  render() {
+    return <div/>
+  }
+}
--- repos/studio-ui/src/StudioRoom.tsx
+++ repos/studio-ui/src/StudioRoom.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import { StudioParticipant } from "./StudioParticipant";
 
 export class StudioRoom extends React.Component<{}, {}> {
   render() {
-    return <strong>Hello, world!</strong>;
+    return (
+      <ul>
+        <li><StudioParticipant/></li>
+        <li><StudioParticipant/></li>
+        <li><StudioParticipant/></li>
+      </ul>
+    );
   }
 }
