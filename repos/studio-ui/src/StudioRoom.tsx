8222e525cd30ff6778939764735cc655eb29840a
Caleb Meredith <calebmeredith8@gmail.com>
2017-04-06 16:18:22 -0400

    remove participants

--- repos/studio-ui/src/StudioRoom.tsx
+++ repos/studio-ui/src/StudioRoom.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import { StudioParticipant } from "./StudioParticipant";
 
 export class StudioRoom extends React.Component<{}, {}> {
   render() {
     return (
       <ul>
-        <li><StudioParticipant/></li>
-        <li><StudioParticipant/></li>
-        <li><StudioParticipant/></li>
       </ul>
     );
   }
