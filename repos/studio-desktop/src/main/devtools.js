b48c15c6633968b59597999cacb4b3da808e8c71
Caleb Meredith <calebmeredith8@gmail.com>
2017-04-14 18:33:40 -0400

    Revert "reorganize repos"
    This reverts commit 14d8c5f56296c81f20d4b45312a257c569e36875.

---
+++ repos/studio-desktop/src/main/devtools.js
@@ -0,0 +1,3 @@
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+
+installExtension(REACT_DEVELOPER_TOOLS, false);
---
+++ repos/studio-signal-exchange/src/client/index.ts
@@ -0,0 +1,2 @@
+export { SignalClient } from './SignalClient';
+export { Signal, OfferSignal, AnswerSignal, CandidateSignal } from '../shared/MessageTypes';
